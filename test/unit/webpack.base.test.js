e97e3b827885eb1aa1b2598a534c41cd4f6d97a6
Mr.liu <leo.liu@asiahr.com>
2020-09-28 14:45:13 +0800

    chore: update unit test js

--- test/unit/webpack.base.test.js
+++ test/unit/webpack.base.test.js
@@ -2,6 +2,6 @@ const assert = require('assert');
 describe('webpack base config test', () => {
     const baseConfig = require('../../lib/webpack.base');
     it('entry', () => {
-        assert.equal(baseConfig.entry.login, '/Users/liuminghai/Documents/webpacktest/builder-webpack/test/smoke/template/src/login/index.js');
+        assert.equal(baseConfig.entry.login.indexOf('/test/smoke/template/src/login/index.js') > 0, true);
     });
 });
